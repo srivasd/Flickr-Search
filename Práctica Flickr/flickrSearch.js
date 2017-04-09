ab054e2e4716e887cbd32976d13e7c5b12dc2ec6
srivasd <s.rivasd@alumnos.urjc.es>
2017-04-09 22:41:24 +0200

    code refactor

--- Práctica Flickr/flickrSearch.js
+++ Práctica Flickr/flickrSearch.js
@@ -15,27 +15,7 @@ $('#info').html("<img src='spinner.gif'/>");
  	for(var i=0; i<data.photos.photo.length; i++){
 		var photoUrl = "https://farm"+data.photos.photo[i].farm+".staticflickr.com/"+data.photos.photo[i].server+"/"+data.photos.photo[i].id+"_"+data.photos.photo[i].secret;
 		$('<img src="'+ photoUrl +'"/>').load(function() {
-  			$('<button id=\'detail\'><img src="'+ this.src +'_s.jpg"/></button>').appendTo('#info');
-			$('#detail').click(function () {
- 
- 			var photo_id= 33545149930; //Temporary
-			//GET ALL CONTEXTS
- 			$.ajax({
- 				url:"https://api.flickr.com/services/rest/"+
-					"?api_key="+api_key+"&format=json"+
-					"&method=flickr.photos.getAllContexts&photo_id="+photo_id+"&nojsoncallback=?"
- 			}).done(function(data) {
-				console.log(data);
-			});
-			//GET LIST FOR PHOTO
-			$.ajax({
- 				url:"https://api.flickr.com/services/rest/"+
-					"?api_key="+api_key+"&format=json"+
-					"&method=flickr.galleries.getListForPhoto&photo_id="+photo_id+"&nojsoncallback=?"
- 			}).done(function(data) {
-				console.log(data);
-		});
-		});
+  			$('<button id=\"detail\" onclick=\"window.location.href=\''+ this.src +'_b.jpg\'\"><img src="'+ this.src +'_s.jpg"/></button>').appendTo('#info');
 	});
  	}
 });
