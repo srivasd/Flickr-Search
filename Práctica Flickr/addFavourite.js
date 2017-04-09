09828dfa524b5e35376b792193a37c2c193a7eb9
srivasd <s.rivasd@alumnos.urjc.es>
2017-04-09 18:01:46 +0200

    galleries + photos attempt

--- Práctica Flickr/addFavourite.js
+++ Práctica Flickr/addFavourite.js
@@ -1,8 +1,12 @@
-/*$.ajax({
+ /*$(document).ready(function(){
+
+ var photo_id= 33545149930; //Temporary
+
+$.ajax({
  method: "POST",
  url:"https://api.flickr.com/services/rest/"+
 				"?api_key="+api_key+"&format=json"+
-				"&method=flickr.favorites.add&+photo_id="+id+"&nojsoncallback=?",
+				"&method=flickr.favorites.add&+photo_id="+photo_id+"&nojsoncallback=?",
  data: JSON.stringify({}),
  processData: false,
  headers: {
@@ -12,4 +16,5 @@
     console.log(textStatus+" "+jqXHR.statusCode());
  }).fail(function(data, textStatus, jqXHR){
     console.log(textStatus+" "+jqXHR.statusCode());
+ });
  });*/
