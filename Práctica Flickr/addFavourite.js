56563cfeb864332876794c1eb00a3b92477a5ce3
srivasd <s.rivasd@alumnos.urjc.es>
2017-04-11 16:36:03 +0200

    image detail in favourite section fixed

--- Práctica Flickr/addFavourite.js
+++ Práctica Flickr/addFavourite.js
@@ -5,6 +5,6 @@ function addFavourite (value) {
    document.getElementsByClassName(value + "class")[0].style.visibility = 'hidden';
 
     jQuery.data( div, value );
-    $('<div style="width:95px;display:inline-block;margin:5px;"><button id=\"detail\" onclick=\"window.location.href=\''+ value +'_b.jpg\'\"><img src="'+ value +'_s.jpg"/></button</div>').appendTo('#favs');
+    $('<div style="width:95px;display:inline-block;margin:5px;"><button id=\"detail\" onclick=\"seeDetails(\'' + value +'\')\"><img src="'+ value +'_s.jpg"/></button</div>').appendTo('#favs');
 }
     
