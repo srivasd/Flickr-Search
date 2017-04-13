2d7eef488afcf64c01d442a8b4f6fb4bd2e48fe4
ealfageme <e.alfageme@alumnos.urjc.es>
2017-04-13 11:46:15 +0200

    find by gallery

--- Práctica Flickr/flickrSearch.js
+++ Práctica Flickr/flickrSearch.js
@@ -28,6 +28,36 @@ $('#info').html("<img src='spinner.gif'/>");
      $('#info').add("</div>")
 });
 });
+    
+    
+    
+$('#findByGallery').click(function(){
+    var gallery = "house";
+    $.ajax({
+        url:"https://api.flickr.com/services/rest/"+
+				"?api_key="+api_key+"&format=json"+
+				"&method=flickr.photos.search&in_gallery="+gallery+"&nojsoncallback=?"
+        
+    }).done(function(data){
+        openTab(event, 'browser');
+        $('#info').add("<div>");
+        var photoTitle = "";
+        for(var i=0; i< data.photos.photo.length;i++){
+            var photoUrl = "https://farm"+data.photos.photo[i].farm+".staticflickr.com/"+data.photos.photo[i].server+"/"+data.photos.photo[i].id+"_"+data.photos.photo[i].secret;
+            var photoId = data.photos.photo[i].id;
+            photoTitle = data.photos.photo[i].title;
+            $('<img id ="'+ photoTitle +'" src="'+ photoUrl +'"/>').load(function(){
+                
+            var fun = "addFavorite(";
+            let srcc = "1";															
+  			$('<div style="width:95px;display:inline-block;margin:5px;"><button id=\"detail\" onclick=\"seeDetails(\'' + this.src+'\',\''+ this.id +'\')\"><img src="'+ this.src +'_s.jpg"/></button><div  class=\'' + this.src + 'class\'><button id="'+ photoUrl +';" style="position:relative;top:50%; left:25%;margin:5px;" onclick="addFavourite(\'' + this.src + '\',\''+ this.id +'\')">Fav</button></div></div>').appendTo('#info');
+            });
+            
+        }
+        $('#info').add("</div>")
+    });
+    
+});
 });
 
   
